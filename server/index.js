dbf05e7161ed91339aad236685dc52fcd5f25942
Anna Miura <annamaliamiura@gmail.com>
2019-09-27 15:21:26 -0700

    Troubleshoot EB deployment

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ const db = require('../database/index');
 const app = express();
 const port = process.env.PORT || 3001;
 const seedFunctions = require('../database/helperFunctions');
+console.log('TEST', port);
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use('/', proxy());
+
 
 app.get('/programmerHumor', (req, res) => {
   return db.grabMemes('programmerMemesModel')
